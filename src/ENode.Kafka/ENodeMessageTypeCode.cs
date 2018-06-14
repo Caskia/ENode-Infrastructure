00a3a2643e0aad3aac365cf8b9056ed29f246256
Caskia <liubai61@163.com>
2018-06-14 16:02:21 +0800

    fix

---
+++ src/ENode.Kafka/ENodeMessageTypeCode.cs
@@ -0,0 +1,10 @@
+﻿namespace ENode.Kafka
+{
+    public enum ENodeMessageTypeCode
+    {
+        CommandMessage = 1,
+        DomainEventStreamMessage = 2,
+        ExceptionMessage = 3,
+        ApplicationMessage = 4,
+    }
+}
